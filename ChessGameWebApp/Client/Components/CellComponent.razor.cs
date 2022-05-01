53bfea10fbf357dd5f2c2e677225cf5cd3ccbca7
turbosuns7 <turbosuns7@gmail.com>
2022-05-01 15:47:07 +0300

    onclick green

--- ChessGameWebApp/Client/Components/CellComponent.razor.cs
+++ ChessGameWebApp/Client/Components/CellComponent.razor.cs
@@ -5,12 +5,18 @@ namespace ChessGameWebApp.Client.Components
 {
     public class CellComponentModel : ComponentBase
     {
-        public bool IsMarked { get; set; }
+        [Parameter]
+        public bool IsMarked { get; set; } = false;
         [Parameter]
         public int Row { get; set; }
         [Parameter]
         public int Column { get; set; }
         [Parameter]
         public Figure? Figure { get; set; }
+
+        public void Click()
+        {
+            IsMarked = true;
+        }
     }
 }

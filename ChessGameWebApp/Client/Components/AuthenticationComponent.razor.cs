5896f12da2ff73d13cebaa85cd7d082fc2986bcc
turbosuns7 <turbosuns7@gmail.com>
2022-06-26 18:38:33 +0300

    auth page

--- ChessGameWebApp/Client/Components/AuthenticationComponent.razor.cs
+++ ChessGameWebApp/Client/Components/AuthenticationComponent.razor.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using ChessGameWebApp.Client.Pages;
+using Microsoft.AspNetCore.Components;
 using System.ComponentModel.DataAnnotations;
 
 namespace ChessGameWebApp.Client.Components
 {
     public class AuthenticationComponentModel : ComponentBase
     {
+        [Parameter]
+        public AuthorizationModel Parent { get; set; }
         [Required]
         [StringLength(20, MinimumLength = 4, ErrorMessage = "4 - 20 character")]
         public string Password { get; set; }
---
+++ ChessGameWebApp/Client/Pages/Authorization.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ChessGameWebApp.Client.Pages
+{
+    public class AuthorizationModel : ComponentBase
+    {
+        public bool IsShowRegistration { get; private set; }
+        public void MenuSwitch()
+        {
+            IsShowRegistration = !IsShowRegistration;
+        }
+    }
+}

6e1dc91b20bf7e0279600ac917a9c56b064492fa
turbosuns7 <turbosuns7@gmail.com>
2022-09-04 13:21:02 +0300

    pawn refactoring

--- ChessGame/Figures/Pawn.cs
+++ ChessGame/Figures/Pawn.cs
@@ -13,29 +13,43 @@ namespace ChessGame.Figures
         }
         internal override List<Cell> GetAllPossibleMoves()
         {
-            var attackFields = new List<Cell>();
+            var moves = new List<Cell>();
 
             if (Position is not null)
             {
                 var direction = Color == FigureColors.White ? Directions.Up : Directions.Down;
 
-                if (direction == Directions.Up)
-                {
-                    attackFields.AddRange(Position.GetCellsInDirection(Directions.LeftUp, 1));
-                    attackFields.AddRange(Position.GetCellsInDirection(Directions.RightUp, 1));
-                }
-                else
-                {
-                    attackFields.AddRange(Position.GetCellsInDirection(Directions.LeftDown, 1));
-                    attackFields.AddRange(Position.GetCellsInDirection(Directions.RightDown, 1));
-                }
-                
-                attackFields = attackFields.Where(cell => cell.Figure != null && cell.Figure.Color != Color).ToList();
-                int range = IsFirstMove == 0 && (Position.Row == 1 || Position.Row == 6) ? 2 : 1;
-                attackFields.AddRange(Position.GetCellsInDirection(direction, range).Where(i => i.Figure == null));
+                moves.AddRange(AddAttackFields(direction));
+                moves.AddRange(AddForwardFields(direction));
             }
 
-            return attackFields;
+            return moves;
+        }
+
+        private List<Cell> AddAttackFields(Directions direction)
+        {
+            var attackFields = new List<Cell>();
+            if (direction == Directions.Up)
+            {
+                attackFields.AddRange(Position.GetCellsInDirection(Directions.LeftUp, 1));
+                attackFields.AddRange(Position.GetCellsInDirection(Directions.RightUp, 1));
+            }
+            else
+            {
+                attackFields.AddRange(Position.GetCellsInDirection(Directions.LeftDown, 1));
+                attackFields.AddRange(Position.GetCellsInDirection(Directions.RightDown, 1));
+            }
+
+            return attackFields.Where(cell => cell.Figure != null && cell.Figure.Color != Color).ToList();
+        }
+
+        private List<Cell> AddForwardFields(Directions direction)
+        {
+            var moves = new List<Cell>();
+            int range = IsFirstMove == 0 && (Position.Row == 1 || Position.Row == 6) ? 2 : 1;
+            moves.AddRange(Position.GetCellsInDirection(direction, range).Where(i => i.Figure == null));
+
+            return moves;
         }
         internal override Figure Clone() => new Pawn(Color);
     }

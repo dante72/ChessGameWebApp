32931ca4bc533a5b6ae0da878dbc5d923f8052f3
Kutyrev Dmitry <turbosuns@yandex.ru>
2022-12-27 15:29:27 +0300

    fix version bug

--- ChessGame/Figures/Figure.cs
+++ ChessGame/Figures/Figure.cs
@@ -11,7 +11,7 @@ namespace ChessGame
         internal Stack<SavedMove> SavedMoves { set; get; } = new Stack<SavedMove>();
         internal int MovesCount { get; set; }
         public FigureColor Color { get; private set; }
-        public Cell Position { get; set; }
+        public Cell Position;
         internal abstract List<Cell> GetAllPossibleMoves();
         public IEnumerable<Cell> PossibleMoves
         {

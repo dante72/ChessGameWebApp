52aeb23843c3c945fa1c9031b6accfb77d15f59d
Kutyrev Dmitry <turbosuns@yandex.ru>
2022-12-31 12:34:11 +0300

    create di container

---
+++ ChessGameWebApp/Client/Services/ChessGameClient.cs
@@ -0,0 +1,26 @@
+﻿using AuthWebAPI;
+using Castle.Windsor;
+using ChessGameWebApp.Client.Models;
+using ChessGameWebApp.Client.Services.Impl;
+
+namespace ChessGameWebApp.Client.Services
+{
+    public class ChessGameClient
+    {
+        var container = new WindsorContainer();
+        static IAuthWebApi authWebApi;
+        static SiteUserInfo siteUserInfo;
+        static IMyLocalStorageService localStorageService;
+        static TimeUpdater timeUpdater;
+        static ILogger<AuthServiceImpl> loggerAuthServiceImpl;
+
+        public static IAuthService authService =
+            new AuthServiceImpl(loggerAuthServiceImpl,
+                                authWebApi,
+                                siteUserInfo,
+                                localStorageService,
+                                timeUpdater);
+
+        public IChatHubService chatHubService = new 
+    }
+}
